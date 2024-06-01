66bddff191a630f8bc89a18cad49ff4c75e9c6ac
Wildo Gómez <wgomez@fintechinversiones.com.py>
2024-06-01 18:46:43 -0400

    hola

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userRoutes from "./api/route/ClassRoute";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api", userRoutes);
-
-export default app;
